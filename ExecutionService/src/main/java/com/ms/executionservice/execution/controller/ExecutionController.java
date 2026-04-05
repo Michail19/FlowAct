f4b7955d3c73cfb98da63218495de2e08df13130
Michail19 <Mi.ershov.2017@yandex.ru>
2026-04-05 17:23:49 +0300

    Update ExecutionController.java

--- ExecutionService/src/main/java/com/ms/executionservice/execution/controller/ExecutionController.java
+++ ExecutionService/src/main/java/com/ms/executionservice/execution/controller/ExecutionController.java
@@ -1,12 +1,14 @@
 package com.ms.executionservice.execution.controller;
 
+import com.ms.executionservice.execution.dto.response.ExecutionResponse;
 import com.ms.executionservice.execution.service.ExecutionService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.Valid;
+import org.hibernate.validator.constraints.UUID;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/executions")
+@RequestMapping("/api/v1")
 public class ExecutionController {
 
     private final ExecutionService executionService;
@@ -15,5 +17,32 @@ public class ExecutionController {
         this.executionService = executionService;
     }
 
+    @PostMapping("/worflows/{workflowId}/executions")
+    @ResponseStatus(HttpStatus.CREATED)
+    public ExecutionResponse run(
+            @PathVariable UUID workflowId,
+            @Valid @RequestBody RunExecutionRequest request
+    ) {
+        return executionService.run(workflowId, request);
+    }
 
+    @GetMapping("/executions/{executionId}")
+    public ExecutionResponse getById(@PathVariable UUID executionId) {
+        return executionService.getById(executionId);
+    }
+
+    @GetMapping("/executions/{executionId}/nodes")
+    public List<NodeExecutionResponse> getNodeExecutions(@PathVariable UUID executionId) {
+        return executionService.getNodeExecutions(executionId);
+    }
+
+    @PostMapping("/executions/{executionId}/retry")
+    public ExecutionResponse retry(@PathVariable UUID executionId) {
+        return executionService.retry(executionId);
+    }
+
+    @PostMapping("/executions/{executionId}/cancel")
+    public ExecutionResponse cancel(@PathVariable UUID executionId) {
+        return executionService.cancel(executionId);
+    }
 }
