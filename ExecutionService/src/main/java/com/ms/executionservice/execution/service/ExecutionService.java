ddb1a18098dc850dc44feeb2708b0293ccec15af
Michail19 <Mi.ershov.2017@yandex.ru>
2026-04-06 14:09:32 +0300

    Implementation

--- ExecutionService/src/main/java/com/ms/executionservice/execution/service/ExecutionService.java
+++ ExecutionService/src/main/java/com/ms/executionservice/execution/service/ExecutionService.java
@@ -1,7 +1,25 @@
 package com.ms.executionservice.execution.service;
 
+import com.ms.executionservice.execution.dto.request.CreateExecutionRequest;
+import com.ms.executionservice.execution.dto.response.ExecutionLogResponse;
+import com.ms.executionservice.execution.dto.response.ExecutionResponse;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class ExecutionService {
+
+    public ExecutionResponse run(UUID workflowId, CreateExecutionRequest request) {}
+
+    public ExecutionResponse getById(UUID executionId) {}
+
+    public List<ExecutionLogResponse> getLogs(UUID executionId) {}
+
+    public List<ExecutionResponse> getExecutionsByWorkflow(UUID workflowId) {}
+
+    public ExecutionResponse retry(UUID executionId) {}
+
+    public ExecutionResponse cancel(UUID executionId) {}
 }
--- ExecutionService/src/main/java/com/ms/executionservice/workflow/service/WorkflowService.java
+++ ExecutionService/src/main/java/com/ms/executionservice/workflow/service/WorkflowService.java
@@ -1,7 +1,28 @@
 package com.ms.executionservice.workflow.service;
 
+import com.ms.executionservice.workflow.dto.request.CreateWorkflowRequest;
+import com.ms.executionservice.workflow.dto.request.UpdateWorkflowRequest;
+import com.ms.executionservice.workflow.dto.response.WorkflowResponse;
+import com.ms.executionservice.workflow.dto.response.WorkflowValidationResponse;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
 
 @Service
 public class WorkflowService {
+
+    public WorkflowResponse create(CreateWorkflowRequest request) {}
+
+    public WorkflowResponse getById(UUID workflowId) {}
+
+    public List<WorkflowResponse> getAll() {}
+
+    public WorkflowResponse update(UUID workflowId, UpdateWorkflowRequest request) {}
+
+    public WorkflowValidationResponse validate(UUID workflowId) {}
+
+    public WorkflowResponse activate(UUID workflowId) {}
+
+    public WorkflowResponse archive(UUID workflowId) {}
 }
