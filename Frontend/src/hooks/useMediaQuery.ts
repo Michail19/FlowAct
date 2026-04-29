c351c6de1b83fee3a2273a392f5abf80df4b265e
Michail19 <Mi.ershov.2017@yandex.ru>
2026-04-29 12:03:57 +0300

    Update useMediaQuery.ts

---
+++ Frontend/src/hooks/useMediaQuery.ts
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react';
+
+export function useMediaQuery(query: string): boolean {
+    const getMatches = () => {
+        if (typeof window === 'undefined') {
+            return false;
+        }
+
+        return window.matchMedia(query).matches;
+    };
+
+    const [matches, setMatches] = useState(getMatches);
+
+    useEffect(() => {
+        const mediaQueryList = window.matchMedia(query);
+
+        const handleChange = () => {
+            setMatches(mediaQueryList.matches);
+        };
+
+        handleChange();
+
+        mediaQueryList.addEventListener('change', handleChange);
+
+        return () => {
+            mediaQueryList.removeEventListener('change', handleChange);
+        };
+    }, [query]);
+
+    return matches;
+}
