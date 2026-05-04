d0018d26d85cbbf16b4cc4c82eec60ea5e4b373f
Michail19 <Mi.ershov.2017@yandex.ru>
2026-05-04 12:11:07 +0300

    Squashed commit of the following:
    commit a50e3a7a1e7dfac9add10305a36b2237654b31fb
    Author: Michail <62212858+Michail19@users.noreply.github.com>
    Date:   Mon May 4 12:02:14 2026 +0300
    
        Add Flyway PostgreSQL database module
    
    commit 7c07b8531d4d93d4f9ef27735cf62eeddbcff8b6
    Author: Michail <62212858+Michail19@users.noreply.github.com>
    Date:   Mon May 4 11:58:05 2026 +0300
    
        Add Flyway init test data migration
    
    commit ddc566b5d45ae4ed4c252feba353d3d31a15100e
    Author: Michail <62212858+Michail19@users.noreply.github.com>
    Date:   Mon May 4 11:55:15 2026 +0300
    
        Use Flyway for init test data
    
    commit 7675876e39676c39dcb7463a493d262336a4aa32
    Author: Michail <62212858+Michail19@users.noreply.github.com>
    Date:   Mon May 4 11:50:11 2026 +0300
    
        Disable duplicate workflow notebook entity
    
    commit 95dc9c2488c64c35c9d5034a833c2d85cb14d93e
    Author: Michail <62212858+Michail19@users.noreply.github.com>
    Date:   Mon May 4 11:48:27 2026 +0300
    
        Use notebooks repository in workflow service
    
    commit 22807d2015a93a65763871c714d1c26aeeea1368
    Author: Michail <62212858+Michail19@users.noreply.github.com>
    Date:   Mon May 4 11:47:50 2026 +0300
    
        Use notebooks entity in workflow
    
    commit ba9819c6a025b338bc14a3159caee329bcbc88cb
    Author: Michail <62212858+Michail19@users.noreply.github.com>
    Date:   Mon May 4 11:44:54 2026 +0300
    
        Remove duplicate workflow notebook repository

---
+++ ExecutionService/src/main/java/com/ms/executionservice/workflow/entity/NotebookEntity.java
@@ -0,0 +1,10 @@
+package com.ms.executionservice.workflow.entity;
+
+/**
+ * Legacy placeholder kept only to avoid stale references in older builds.
+ * The canonical notebook entity is com.ms.executionservice.notebooks.entity.NotebookEntity.
+ */
+final class LegacyWorkflowNotebookEntity {
+    private LegacyWorkflowNotebookEntity() {
+    }
+}
