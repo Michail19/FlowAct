7965dd98cec882c760e9a7b750dacc8ce322e1b5
Michail <62212858+Michail19@users.noreply.github.com>
2026-05-13 10:39:54 +0300

    feat(user-service): add refresh token request dto

---
+++ UserService/src/main/java/com/ms/userservice/auth/dto/request/RefreshTokenRequest.java
@@ -0,0 +1,9 @@
+package com.ms.userservice.auth.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record RefreshTokenRequest(
+        @NotBlank
+        String refreshToken
+) {
+}
