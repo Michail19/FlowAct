4147d56adee7399eb4017bc5fb9317fb7aaca9b4
Michail <62212858+Michail19@users.noreply.github.com>
2026-05-13 18:16:05 +0300

    feat(execution): read notebook user from jwt

--- ExecutionService/src/main/java/com/ms/executionservice/notebooks/controller/NotebookController.java
+++ ExecutionService/src/main/java/com/ms/executionservice/notebooks/controller/NotebookController.java
@@ -5,8 +5,10 @@ import com.ms.executionservice.notebooks.dto.request.UpdateNotebookRequest;
 import com.ms.executionservice.notebooks.dto.response.NotebookResponse;
 import com.ms.executionservice.notebooks.dto.response.NotebookShortResponse;
 import com.ms.executionservice.notebooks.service.NotebookService;
+import com.ms.executionservice.security.util.CurrentUserUtils;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -25,42 +27,45 @@ public class NotebookController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public NotebookResponse create(
-            @RequestHeader("X-User-Id") UUID userId,
+            Authentication authentication,
             @Valid @RequestBody CreateNotebookRequest request
     ) {
+        UUID userId = CurrentUserUtils.getCurrentUserId(authentication);
         return notebookService.create(userId, request);
     }
 
     @GetMapping("/{notebookId}")
     public NotebookResponse getById(
-            @RequestHeader("X-User-Id") UUID userId,
+            Authentication authentication,
             @PathVariable UUID notebookId
     ) {
+        UUID userId = CurrentUserUtils.getCurrentUserId(authentication);
         return notebookService.getById(userId, notebookId);
     }
 
     @GetMapping
-    public List<NotebookShortResponse> getAll(
-            @RequestHeader("X-User-Id") UUID userId
-    ) {
+    public List<NotebookShortResponse> getAll(Authentication authentication) {
+        UUID userId = CurrentUserUtils.getCurrentUserId(authentication);
         return notebookService.getAll(userId);
     }
 
     @PutMapping("/{notebookId}")
     public NotebookResponse update(
-            @RequestHeader("X-User-Id") UUID userId,
+            Authentication authentication,
             @PathVariable UUID notebookId,
             @Valid @RequestBody UpdateNotebookRequest request
     ) {
+        UUID userId = CurrentUserUtils.getCurrentUserId(authentication);
         return notebookService.update(userId, notebookId, request);
     }
 
     @DeleteMapping("/{notebookId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(
-            @RequestHeader("X-User-Id") UUID userId,
+            Authentication authentication,
             @PathVariable UUID notebookId
     ) {
+        UUID userId = CurrentUserUtils.getCurrentUserId(authentication);
         notebookService.delete(userId, notebookId);
     }
 }
