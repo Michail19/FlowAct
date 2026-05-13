92c41c10274f055e3e07ad1d21e63046786f8bbd
Michail <62212858+Michail19@users.noreply.github.com>
2026-05-13 21:51:16 +0300

    feat(user-service): add change password endpoint

--- UserService/src/main/java/com/ms/userservice/users/controller/UserController.java
+++ UserService/src/main/java/com/ms/userservice/users/controller/UserController.java
@@ -1,15 +1,19 @@
 package com.ms.userservice.users.controller;
 
 import com.ms.userservice.security.util.CurrentUserUtils;
+import com.ms.userservice.users.dto.request.UpdateCredentialsRequest;
 import com.ms.userservice.users.dto.request.UpdateCurrentUserRequest;
 import com.ms.userservice.users.dto.response.UserResponse;
 import com.ms.userservice.users.service.UserService;
 import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
@@ -37,5 +41,15 @@ public class UserController {
     ) {
         UUID userId = CurrentUserUtils.getCurrentUserId(authentication);
         return userService.updateCurrentUser(userId, request);
+    }
+
+    @PostMapping("/me/change-password")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void changePassword(
+            Authentication authentication,
+            @Valid @RequestBody UpdateCredentialsRequest request
+    ) {
+        UUID userId = CurrentUserUtils.getCurrentUserId(authentication);
+        userService.updateCredentials(userId, request);
     }
 }
